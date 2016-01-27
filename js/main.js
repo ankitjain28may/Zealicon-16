40de26e166a73aa9e4e2c3af055d6a8af91bb6d8
Ankit Jain <ankitjain28may77@gmail.com>
2016-01-27 14:28:21 +0530

    Change

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var orange = [40, 100, 60];
 var green = [75, 100, 40];
 var blue = [196, 77, 55];
 var purple = [280, 50, 60];
-var letterColors = [black, orange, green, blue, purple];
+var letterColors = [red,white,blue,green,green, blue,white,red];
 
 drawName(myName, letterColors);
 
