2d67ce5e394f1630dbb5dea03b101f028cddc4ab
Ankit Jain <ankitjain28may77@gmail.com>
2016-01-27 14:12:57 +0530

    Starting

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var myName = "Zealicon";
+
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
+var letterColors = [black, orange, green, blue, purple];
+
+drawName(myName, letterColors);
+
+if(10 > 3)
+{
+    bubbleShape = '1';
+}
+else
+{
+    bubbleShape = '0';
+}
+
+bounceBubbles();
